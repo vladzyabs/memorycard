1d4fb6e638775b226542a3a657bd2da2279f3edd
vladz <vladzyaba@gmail.com>
2020-10-01 21:24:56 +0300

    ui Restore password page

--- src/pages/RestorePassword/RestorePassword.tsx
+++ src/pages/RestorePassword/RestorePassword.tsx
@@ -1,33 +1,63 @@
-import React, {ChangeEvent} from 'react'
+import React from 'react'
+import classes from './RestorePassword.module.scss'
 import {Button, Input} from '../../components'
 import {useDispatch, useSelector} from 'react-redux'
 import {getEmailConfirmation} from '../../store/restorePasswordReducer/restorePasswordAction'
 import {AppRootStateType} from '../../store/store'
+import * as Yup from 'yup'
+import {useFormik} from 'formik'
+
+const validationSchema = Yup.object({
+   email: Yup.string()
+      .required('Empty field')
+      .email('Invalid email address'),
+})
 
 const RestorePassword = () => {
 
    const dispatch = useDispatch()
-   const selectedEmail = useSelector<AppRootStateType>(state => state.restorePassword.selectedEmail)
-   const [email, setEmail] = React.useState<string>('')
-
-   const handleEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
-
-   const onClickSend = () => {
-      dispatch(getEmailConfirmation(email))
-   }
+   const selectedEmail = useSelector<AppRootStateType, string>(state => state.restorePassword.selectedEmail)
+   const formik = useFormik({
+      initialValues: {
+         email: '',
+      },
+      validationSchema,
+      onSubmit: values => {
+         dispatch(getEmailConfirmation(values.email))
+      },
+   })
 
    return (
-      <div>
+      <div className={classes.page}>
          <h1>Restore password</h1>
 
          {
-            selectedEmail && <p><a href={`mailto:${selectedEmail}`}>Email</a></p>
+            selectedEmail && <p className={classes.successMessage}>
+                Check your email:{' '}
+                <a href={`mailto:${selectedEmail}`}>{selectedEmail}</a>{' '}
+                and follow the link in there
+            </p>
          }
 
-         <Input type={'text'} inputTitle={'Email'} value={email} onChange={handleEmail}/>
-         <Button onClick={onClickSend}>Send</Button>
+         <form onSubmit={formik.handleSubmit}>
+
+            <div className={classes.formItem}>
+               <Input type={'text'}
+                      inputTitle={'Email:'}
+                      error={formik.touched.email ? formik.errors.email : ''}
+                      {...formik.getFieldProps('email')}
+               />
+            </div>
+
+            <div className={classes.formItem}>
+               <Button type={'submit'}
+                       disabled={!!formik.errors.email}
+               >Send</Button>
+            </div>
+
+         </form>
       </div>
    )
 }
 
-export default RestorePassword
+export default React.memo(RestorePassword)
